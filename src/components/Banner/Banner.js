bf899bdb335b545a7fa7c86fa6e5f71bc9755ffe
requiem-of-zero <swonggzz@gmail.com>
2022-12-30 13:40:58 -0800

    Added comments for readability

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -19,10 +19,12 @@ const Banner = ({ netflixOriginals }) => {
 
   return (
     <div className="relative flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end">
-      <div className="z-0 absolute top-0 left-0 h-[50vh] lg:h-[95vh] w-screen select-none">
+      {/* Banner Backdrop Image */}
+      <div className="z-0 absolute top-0 left-0 h-[70vh] lg:h-[95vh] w-screen select-none">
         <Image
           src={`${IMAGE_BASE_URL}/${
-            featuredMovie && featuredMovie.backdrop_path || featuredMovie?.poster_path
+            (featuredMovie && featuredMovie.backdrop_path) ||
+            featuredMovie?.poster_path
           }`}
           object-fit="contain"
           fill
@@ -32,16 +34,18 @@ const Banner = ({ netflixOriginals }) => {
           alt="featured backdrop"
         />
       </div>
+      {/* Banner Content Container */}
       <div className="bannerCTA z-10 pl-10 lg:w-200">
         <h1 className="font-bold max-w-s text-2xl pb-4 text-shadow-md md:text-4xl lg:text-7xl">
-          {(featuredMovie && featuredMovie.title )|| (featuredMovie &&
-            featuredMovie.name )|| (featuredMovie &&
-            featuredMovie.original_title)}
+          {(featuredMovie && featuredMovie.title) ||
+            (featuredMovie && featuredMovie.name) ||
+            (featuredMovie && featuredMovie.original_title)}
         </h1>
         <p className="max-w-s pb-5 text-shadow-md text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
           {featuredMovie && featuredMovie.overview}
         </p>
         <div className="flex space-x-3">
+          {/* Banner Buttons */}
           <button className="bannerBtn bg-white text-black">
             <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
           </button>
