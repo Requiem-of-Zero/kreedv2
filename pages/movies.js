ae85924f9277c6e9c7de59b645cf31edd5e814e7
requiem-of-zero <swonggzz@gmail.com>
2022-12-30 13:15:07 -0800

    Added movies page and requests to tmdb

---
+++ pages/movies.js
@@ -0,0 +1,90 @@
+import Head from "next/head";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { modalState } from "../atoms/modalAtom";
+import useAuth from "../hooks/useAuth";
+import Banner from "../src/components/Banner/Banner";
+import Header from "../src/components/Header/Header";
+import MediaRows from "../src/components/MediaRow/MediaRows";
+import Modal from "../src/components/Modal/Modal";
+import requests from "../utils/requests";
+
+export const getServerSideProps = async () => {
+  const [
+    historyMovies,
+    musicalMovies,
+    fantasyMovies,
+    mysteryMovies,
+    warMovies,
+    televisionMovies,
+    westernMovies,
+    familyMovies,
+  ] = await Promise.all([
+    fetch(requests.fetchHistoryMovies.url).then((res) => res.json()),
+    fetch(requests.fetchMusicMovies.url).then((res) => res.json()),
+    fetch(requests.fetchFantasyMovies.url).then((res) => res.json()),
+    fetch(requests.fetchMysteryMovies.url).then((res) => res.json()),
+    fetch(requests.fetchWarMovies.url).then((res) => res.json()),
+    fetch(requests.fetchTVMovies.url).then((res) => res.json()),
+    fetch(requests.fetchWesternMovies.url).then((res) => res.json()),
+    fetch(requests.fetchFamilyMovies.url).then((res) => res.json()),
+  ]);
+
+  return {
+    props: {
+      historyMovies: historyMovies.results,
+      musicalMovies: musicalMovies.results,
+      fantasyMovies: fantasyMovies.results,
+      mysteryMovies: mysteryMovies.results,
+      warMovies: warMovies.results,
+      televisionMovies: televisionMovies.results,
+      westernMovies: westernMovies.results,
+      familyMovies: familyMovies.results,
+    },
+  };
+};
+
+const TVShows = ({
+  historyMovies,
+  musicalMovies,
+  fantasyMovies,
+  mysteryMovies,
+  warMovies,
+  televisionMovies,
+  westernMovies,
+  familyMovies,
+}) => {
+  const { logout, loading } = useAuth();
+  const [showModal, setShowModal] = useRecoilState(modalState);
+
+  if (loading) return null;
+
+  return (
+    <div
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && "h-screen overflow-hidden"
+      }`}
+    >
+      <Head>
+        <title>Kreed</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main>
+        <Banner netflixOriginals={historyMovies} />
+        <section className="absolute md:space-y-24 md:pt-40">
+          <MediaRows title="Musical" movies={musicalMovies} />
+          <MediaRows title="Fantasy" movies={fantasyMovies} />
+          <MediaRows title="Mystery" movies={mysteryMovies} />
+          {/* My Watchlist Component */}
+          <MediaRows title="War" movies={warMovies} />
+          <MediaRows title="TV" movies={televisionMovies} />
+          <MediaRows title="Western" movies={westernMovies} />
+          <MediaRows title="Family" movies={familyMovies} />
+        </section>
+      </main>
+      {showModal && <Modal />}
+    </div>
+  );
+};
+
+export default TVShows;
--- utils/requests.js
+++ utils/requests.js
@@ -1,7 +1,8 @@
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
-
+// https://api.themoviedb.org/3/genre/movie/list?api_key=d12daebcca9553d839743717b41fa0d2&language=en-US
 const requests = {
+  // Index page requests
   fetchTrending: {
     title: "Trending",
     url: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
@@ -34,6 +35,7 @@ const requests = {
     title: "Documentaries",
     url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`,
   },
+  //  TV Show Page Requests
   fetchActionTVShows: {
     title: "Action & Adventure",
     url: `${BASE_URL}/discover/tv?api_key=${API_KEY}&language=en-US&with_genres=10759`,
@@ -66,5 +68,38 @@ const requests = {
     title: "Sci-Fi & Fantasy",
     url: `${BASE_URL}/discover/tv?api_key=${API_KEY}&language=en-US&with_genres=10765`,
   },
+  // Movie Page Requests
+  fetchHistoryMovies: {
+    title: 'History',
+    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=36`
+  },
+  fetchMusicMovies: {
+    title: 'Music',
+    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10402`
+  },
+  fetchFantasyMovies: {
+    title: 'Fantasy',
+    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=14`
+  },
+  fetchMysteryMovies: {
+    title: 'Mystery',
+    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=9648`
+  },
+  fetchWarMovies: {
+    title: 'War',
+    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10752`
+  },
+  fetchTVMovies: {
+    title: 'TV Movies',
+    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10770`
+  },
+  fetchWesternMovies: {
+    title: 'Western',
+    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=37`
+  },
+  fetchFamilyMovies: {
+    title: 'Family',
+    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10751`
+  }
 };
 export default requests;
