7bdf938342a7aa4b188a190a1934268875aa2bf0
requiem-of-zero <swonggzz@gmail.com>
2022-12-30 22:36:12 -0800

    Fix deployment

--- constants/media.js
+++ constants/media.js
@@ -1,6 +1,3 @@
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
-export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? window.location.origin
-    : `http://localhost:3000`;
+export const BASE_URL = `http://localhost:3000`;
