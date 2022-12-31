f02b57eab8c9c4c9ef6b0bde4c4a213bc8c80ee6
requiem-of-zero <swonggzz@gmail.com>
2022-12-30 22:26:40 -0800

    Change base url for requests to match the base url for origin

--- constants/media.js
+++ constants/media.js
@@ -2,5 +2,5 @@ export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? `https://kreedv2.vercel.app`
+    ? window.location.origin
     : `http://localhost:3000`;
