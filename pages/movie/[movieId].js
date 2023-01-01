70867b7ed22c9587f101ba0c41bfb7c1a499c211
requiem-of-zero <swonggzz@gmail.com>
2023-01-01 15:19:32 -0800

    Added to movie show page

--- pages/movie/[movieId].js
+++ pages/movie/[movieId].js
@@ -4,12 +4,23 @@ import { useRecoilState } from "recoil";
 import { modalAtomState } from "../../atoms/modalAtom";
 import Header from "../../src/components/Header/Header";
 import Modal from "../../src/components/Modal/Modal";
+import { fetchMovie } from "../../utils/movies";
 
-const MovieShow = () => {
+const getServerSideProps = async () => {
   const router = useRouter();
-  const [showModal, setShowModal] = useRecoilState(modalAtomState);
-  const {movieId} = router.query
+  const movieId = router.query;
+  const movie = fetch(`https://api.themoviedb.org/3/movie/${movieId}`).then(
+    (res) => res.json()
+  );
+  props: {
+    movie: movie;
+  }
+};
 
+const MovieShow = ({movie}) => {
+  const [showModal, setShowModal] = useRecoilState(modalAtomState);
+  
+  console.log(movie);
   return (
     <div>
       <Head>
@@ -17,12 +28,10 @@ const MovieShow = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
-        
-      </main>
+      <main></main>
       {showModal && <Modal />}
     </div>
   );
-}
+};
 
-export default MovieShow
+export default MovieShow;
