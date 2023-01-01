f2ef5b9399fb71909356a46151b1f0c0cb3fe4e7
requiem-of-zero <swonggzz@gmail.com>
2023-01-01 15:46:37 -0800

    Added server side props to movie show page

--- pages/movie/[movieId].js
+++ pages/movie/[movieId].js
@@ -4,22 +4,29 @@ import { useRecoilState } from "recoil";
 import { modalAtomState } from "../../atoms/modalAtom";
 import Header from "../../src/components/Header/Header";
 import Modal from "../../src/components/Modal/Modal";
-import { fetchMovie } from "../../utils/movies";
+import { fetchMovie, getMovieSpecificFromTMDB } from "../../utils/movies";
 
-const getServerSideProps = async () => {
-  const router = useRouter();
-  const movieId = router.query;
-  const movie = fetch(`https://api.themoviedb.org/3/movie/${movieId}`).then(
-    (res) => res.json()
-  );
-  props: {
-    movie: movie;
-  }
+export const getServerSideProps = async (context) => {
+  const movieId = context.query.movieId;
+ 
+  const [movie] = await Promise.all([
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`
+    ).then((res) => res.json()),
+  ]);
+
+  return {
+    props: {
+      movie: movie
+    },
+  };
 };
 
-const MovieShow = ({movie}) => {
+const MovieShow = ({ movie }) => {
+  const router = useRouter();
+  const { movieId } = router.query;
   const [showModal, setShowModal] = useRecoilState(modalAtomState);
-  
+
   console.log(movie);
   return (
     <div>
