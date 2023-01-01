c72a2c3b6245bcf17810cb8691fff035af42ac8f
requiem-of-zero <swonggzz@gmail.com>
2023-01-01 14:58:14 -0800

    Started movie show page

--- pages/movie/[movieId].js
+++ pages/movie/[movieId].js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
+import { modalAtomState } from "../../atoms/modalAtom";
 import Header from "../../src/components/Header/Header";
+import Modal from "../../src/components/Modal/Modal";
 
 const MovieShow = () => {
   const router = useRouter();
-
+  const [showModal, setShowModal] = useRecoilState(modalAtomState);
   const {movieId} = router.query
 
   return (
@@ -13,6 +16,11 @@ const MovieShow = () => {
         <title>Kreed</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
+      <main>
+        
+      </main>
+      {showModal && <Modal />}
     </div>
   );
 }
