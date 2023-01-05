629572c9defff2b9c4b1dbd2a1b2ed848fbafded
requiem-of-zero <swonggzz@gmail.com>
2023-01-05 01:26:07 -0800

    Added progress bar to the entire site for UI/UX

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import { Router } from "next/router";
+import NProgress from "nprogress";
 import { RecoilRoot } from "recoil";
 import { AuthProvider } from "../hooks/useAuth";
 import "../styles/globals.css";
+import '../styles/nprogress.css'; //styles of nprogress
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({ Component, pageProps }) {
   return (
